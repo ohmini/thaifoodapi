1de48fd2cbd35d8a017f4f933dfebf194d75f3d3
Ratthasorn Thassana <ratthasorn.thassana@gmail.com>
2016-05-10 21:47:28 +0700

    add embeded light sensor project

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,8 @@
 (function($) {
     'use strict';
 
-    var baseUrl = 'https://thaifoodapi.appspot.com/';
+    var baseUrl = 'https://thaifoodapi.appspot.com/api/foods/';
+
     var url = baseUrl;
 
     $('#url').text(baseUrl);
@@ -9,12 +10,14 @@
     var searchByElement = $('#searchBy');
     var inputIdElement = $('#searchId');
     var urlText = $('#url');
+    var limitElement = $('#limit');
+    var offsetElement = $('#offset');
 
     searchByElement.change(function(){
         var val = $(this).val();
         var param = '';
         url = baseUrl;
-        if(val === 'ingredient'){
+        if(val === 'ingredient' && inputIdElement.val() ){
             param = '?ingredients='
         }
         url += param;
@@ -25,11 +28,69 @@
         setIdUrl();
     })
 
+    
+
+    function isNoneFirstParam(){
+        setIdUrl();
+        var tempUrl = urlText.text();
+        if(tempUrl.search('?') === -1){
+            return true;
+        }
+        return false;
+    }
+
+
+
+    $('#submit').click(function(){
+        setIdUrl();
+        $.ajax({
+            method: 'GET',
+            url: urlText.text()
+        }).done(function(result){
+            console.log(result);
+            if(result.results){
+                displayData(result.results);
+            }else if(result.id){
+                displayOneData(result);
+            }else{
+                $('#data').children().remove();
+            }
+        })
+
+    })
+
     function setIdUrl(){
         var val = inputIdElement.val();
         var idUrl = url;
         idUrl += val;
         urlText.text(idUrl);
+    }
+
+    function displayData(data){
+        $('#data').children().remove()
+        $.each(data, function(index, value){
+            addRow(value);
+        })
+
+    }
+
+    function displayOneData(data){
+        $('#data').children().remove();
+        addRow(data);
+    }
+
+    function addRow(value){
+
+        var jsonUrl = baseUrl + value.id + "/?format=json";
+        var element = "<tr><td>";
+        element += value.id + "</td><td>";
+        element += value.name + "</td><td>";
+        element += value.calories + "</td><td>";
+        element += "<a class='btn btn-info btn-sm' href='";
+        element += jsonUrl + "' >";
+        element += "json</a></td>";
+        $('#data').append(element);
+
     }
 
 }(jQuery));
