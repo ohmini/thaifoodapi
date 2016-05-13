f037779f70b0843732f0df7a2d5431d3345526a7
Ratthasorn Thassana <ratthasorn.thassana@gmail.com>
2016-05-13 14:52:03 +0700

    add generate url function

--- static/js/main.js
+++ static/js/main.js
@@ -3,46 +3,57 @@
 
     var baseUrl = 'https://thaifoodapi.appspot.com/api/foods/';
 
+    var minNutrientSelection = [
+        "minwater", "minprotien", "minfat", "mincarbohydrate", "mindiearyfiber",
+        "minash", "mincalcium", "minphosphorus", "miniron", "minretinol",
+        "minbetacarotene", "minvitamin_a", "minvitamin_e", "minthiamin", "minriboflavin",
+        "minniacin", "minvitamin_c"
+    ]
+    var maxNutrientSelection = [
+        "maxwater", "minprotien", "maxfat", "maxcarbohydrate", "maxdiearyfiber",
+        "maxash", "maxcalcium", "maxphosphorus", "maxiron", "maxretinol",
+        "maxbetacarotene", "maxvitamin_a", "maxvitamin_e", "maxthiamin", "maxriboflavin",
+        "maxniacin", "maxvitamin_c"
+    ]
+
+    addOption(minNutrientSelection, "#minNutrients");
+    addOption(maxNutrientSelection, "#maxNutrients")
+
+
     var url = baseUrl;
 
     $('#url').text(baseUrl);
 
-    var searchByElement = $('#searchBy');
     var inputIdElement = $('#searchId');
     var urlText = $('#url');
     var limitElement = $('#limit');
     var offsetElement = $('#offset');
+    var minCaloriesElement = $('#minCalories');
+    var maxCaloriesElement = $('#maxCalories');
+    var earthElement = $('#earth');
+    var waterElement = $('#water');
+    var windElement = $('#wind');
+    var fireElement = $('#fire');
+    var excludeElement = $('#exclude');
+    var randomElement = $('#random');
 
-    searchByElement.change(function(){
-        var val = $(this).val();
-        var param = '';
-        url = baseUrl;
-        if(val === 'ingredient' && inputIdElement.val() ){
-            param = '?ingredients='
-        }
-        url += param;
-        setIdUrl();
-    })
 
-    inputIdElement.change(function(){
-        setIdUrl();
-    })
-
-    
-
-    function isNoneFirstParam(){
-        setIdUrl();
-        var tempUrl = urlText.text();
-        if(tempUrl.search('?') === -1){
-            return true;
-        }
-        return false;
-    }
-
+    $('#addMaxNutrients').click(function(){
+        var index = $('#maxNutrients').val();
+        var param = maxNutrientSelection[index];
+        var value = $('#maxNutrientsField').val();
+        addNutrientParameter(param, value);
+    });
+    $('#addMinNutrients').click(function(){
+        var index = $('#minNutrients').val();
+        var param = minNutrientSelection[index];
+        var value = $('#minNutrientsField').val();
+        addNutrientParameter(param, value);
+    });
 
 
     $('#submit').click(function(){
-        setIdUrl();
+        setUrl();
         $.ajax({
             method: 'GET',
             url: urlText.text()
@@ -59,11 +70,89 @@
 
     })
 
-    function setIdUrl(){
-        var val = inputIdElement.val();
-        var idUrl = url;
-        idUrl += val;
-        urlText.text(idUrl);
+    function setUrl(){
+        var params = [];
+        if(inputIdElement.val().length > 0){
+            params.push(createParamObj('ingredients', inputIdElement.val()))
+        }
+        if(limitElement.val().length > 0){
+            params.push(createParamObj('limit', limitElement.val()));
+        }
+        if(offsetElement.val().length > 0){
+            params.push(createParamObj('offset', offsetElement.val()));
+        }
+        if(minCaloriesElement.val().length > 0){
+            params.push(createParamObj('mincalories', minCaloriesElement.val()));
+        }
+        if(maxCaloriesElement.val().length > 0){
+            params.push(createParamObj('maxcalories', maxCaloriesElement.val()));
+        }
+        if(earthElement.is(":checked") || waterElement.is(":checked") ||
+            windElement.is(":checked") || fireElement.is(":checked")
+        ){
+            var val = getElementParamValue();
+            params.push(createParamObj('elements', val));
+        }
+
+        if(excludeElement.is(":checked")){
+            params.push(createParamObj('exclude', 'true'));
+
+        }
+        if(randomElement.is(":checked")){
+            params.push(createParamObj('random', 'true'));
+        }
+
+        console.log(params);
+        var completeUrl = baseUrl + getUrl(params);
+        $('#url').text(completeUrl);
+
+    }
+
+    function getUrl(params){
+        var paramsUrl = '?';
+        $.each(params, function(index, value){
+            paramsUrl += value.key + '=' + value.value;
+            if(index+1 < params.length)
+                paramsUrl += '&';
+        })
+
+        return paramsUrl;
+    }
+
+    function createParamObj(key, value){
+        var obj = {};
+        obj.key = key;
+        obj.value = value;
+        return obj;
+    }
+
+    function getElementParamValue(){
+        var values = [];
+        if(earthElement.is(":checked")){
+            values.push('earth');
+        }
+        if(waterElement.is(":checked")){
+            values.push('water');
+        }
+        if(windElement.is(":checked")){
+            values.push('wind');
+        }
+        if(fireElement.is(":checked")){
+            values.push('fire');
+        }
+        console.log(values);
+
+        var value = '';
+        for(var i=0; i<values.length; i++){
+            value += values[i];
+            if(i+1 >= values.length){
+                break;
+            }else{
+                value += ',';
+            }
+        }
+
+        return value;
     }
 
     function displayData(data){
@@ -91,6 +180,26 @@
         element += "json</a></td>";
         $('#data').append(element);
 
+    }
+
+    function addOption(options, target_id){
+        $.each(options, function(index, value){
+            var item = "<option value='";
+            item += index;
+            item += "'>" + value;
+            item += "</option>";
+            $(target_id).append(item)
+        })
+    }
+
+    function addNutrientParameter(parameter, value){
+        var item = "<li class='list-group-item'>";
+        //item += "<span class='glyphicon glyphicon-minus-sign' style='float:right;cursor:pointer'></span>";
+        item += parameter + '=' + value;
+
+        item += "</li>"
+
+        $('#nutrients').append(item);
     }
 
 }(jQuery));
