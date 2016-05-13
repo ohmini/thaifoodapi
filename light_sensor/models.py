fa8aac8601da41d2d043361a5c21ebe46b6fbd7a
Ratthasorn Thassana <ratthasorn.thassana@gmail.com>
2016-05-13 10:40:11 +0700

    remove cron

--- light_sensor/models.py
+++ light_sensor/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.utils.encoding import python_2_unicode_compatible
 # Create your models here.
+
+
+@python_2_unicode_compatible
+class SensorData(models.Model):
+    entry_id = models.IntegerField(default=0)
+    light_value = models.IntegerField(default=0)
+    alert_value = models.IntegerField(default=300)
+    tel = models.CharField(max_length=20, default='0802618207')
+
+    def __str__(self):
+        return self.entry_id
--- light_sensor/views.py
+++ light_sensor/views.py
@@ -1,5 +1,36 @@
 from django.shortcuts import render
+from light_sensor.models import SensorData
+from datetime import datetime
+import urllib2
+import json
 
 
 def index(request):
+    query_set = SensorData.objects.all().last()
+    response = urllib2.urlopen('http://184.106.153.149/channels/113321/fields/1.json?results=1')
+    data = json.loads(response.read())
+    entry_id = data['feeds'][0]['entry_id']
+    light_value = int(data['feeds'][0]['field1'])
+
+    sms_base_url = 'http://www.thsms.com/api/rest?method=send'
+    sms_username = '&username=ohmini'
+    sms_password = '&password=nros3517'
+    sms_from = '&from=0000'
+    sms_to = '&to=' + query_set.tel
+    sms_msg = '&message=Alert!!'
+
+    sms_get = sms_base_url + sms_username + sms_password + sms_from + sms_to + sms_msg
+
+    if query_set is not None:
+        if query_set.entry_id < entry_id:
+            query_set.light_value = light_value
+            query_set.entry_id = entry_id
+            query_set.save()
+            if light_value > query_set.alert_value:
+                urllib2.urlopen(sms_get)
+
+    else:
+        sensor_data = SensorData(entry_id=0, light_value=0, create_time=datetime.now(), version=0)
+        sensor_data.save()
+
     return render(request, 'embedded/index.html')
